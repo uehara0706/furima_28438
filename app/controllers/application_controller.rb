51a60d69226901b1f398747eaa36527fce710fe0
uehara0706 <68547123+uehara0706@users.noreply.github.com>
2020-08-10 14:25:53 +0900

    リダイレクト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
-  private
-
+  before_action :move_to_index, except: [:index, :show]
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"] 
     end
   end
 end
+
+def move_to_index
+  unless user_signed_in?
+    redirect_to action: :index
+  end

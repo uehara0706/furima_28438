d3063ebfed6de6756102dfe62e33c6e4447bd8c2
uehara0706 <68547123+uehara0706@users.noreply.github.com>
2020-08-11 17:49:10 +0900

    validation修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :email, :password, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date, :password_confirmation, presence: true
+  
+  with_options presence: true do
+  
+  validates :nickname
+  validates :email, uniqueness: {case_sensitive: false},
+                    format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+  validates :password, length: {minimum: 6}
+                       format: {with: /\A[a-z0-9]+\z/i}
+  validates :password_confirmation
+  validates :birth_date
 
+  with_options format: {with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/} do
+       validates :first_name
+       validates :last_name
+     end
+     with_options format: {with: /\A[ァ-ヶー－]+\z/} do
+       validates :first_name_kana
+       validates :last_name_kana
+     end
    end
+ 

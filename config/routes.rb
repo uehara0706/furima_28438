77e6985549d7c60eeb3de1eb13a106048e64eff5
uehara0706 <68547123+uehara0706@users.noreply.github.com>
2020-08-15 17:03:29 +0900

    トップページのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # root 'posts#index'  
-  get 'posts', to: 'posts#index'
+  root 'items#index'  
+  #get 'posts', to: 'posts#index'
   resources :users, only: [:create]  
 end

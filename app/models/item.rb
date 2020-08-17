5f7926e2eb16cd514079200d1617e18e793ab954
uehara0706 <68547123+uehara0706@users.noreply.github.com>
2020-08-17 20:00:58 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
 end
 
+with_options presence: true do
+
+validates :name
+validates :price
+validates :explain
+validates :cate
+validates :state
+validates :burden 
+validates :ship_from
+validates :Shipping_days

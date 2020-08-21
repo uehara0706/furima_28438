bae4f80d2f51d5604c07cbc299549eb6f93484ef
uehara0706 <68547123+uehara0706@users.noreply.github.com>
2020-08-21 11:01:52 +0900

    テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,24 +1,20 @@
 class Item < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
+  # extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one_attached :image
 
-with_options presence: true do
+  with_options presence: true do
 
-validates :name
-validates :price
-validates :explain
-validates :cate_id
-validates :state_id
-validates :burden_id
-validates :ship_form_id
-validates :shipping_days_id
-validates :cate_id, numericality: { other_than: 1 } 
-validates :burden_id, numericality: { other_than: 1 } 
-validates :state_id, numericality: { other_than: 1 } 
-validates :ship_form_id, numericality: { other_than: 1 } 
-validates :shipping_days_id, numericality: { other_than: 1 } 
-validates :price, :numericality => { :greater_than_or_equal_to => 300 }
-validates :price, :numericality => { :less_than_or_equal_to => 9999999 } 
-end
+  validates :name
+  validates :price
+  validates :explain
+  validates :cate_id, numericality: { other_than: 1 } 
+  validates :burden_id, numericality: { other_than: 1 } 
+  validates :state_id, numericality: { other_than: 1 } 
+  validates :ship_form_id, numericality: { other_than: 1 } 
+  validates :shipping_days_id, numericality: { other_than: 1 } 
+  validates :price, :numericality => { :greater_than_or_equal_to => 300 }
+  validates :price, :numericality => { :less_than_or_equal_to => 9999999 } 
+  end
+
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
-  end
+  name             {"a"}
+  price            {"1000"}
+  explain          {"aa"}
+  cate_id          {"2"}
+  state_id         {"2"}
+  burden_id        {"2"}
+  ship_form_id     {"2"}
+  shipping_days_id {"2"}
+  user_id          {"2"}  
+end
 end

d14f3c4e3d438ef0fc85f6c129b5d1d21de0b9fc
uehara0706 <68547123+uehara0706@users.noreply.github.com>
2020-08-29 20:40:28 +0900

    商品購入ページの実装

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,6 @@
+class PurchasesController < ApplicationController
+
+def index
+  @item = Item.find(params[:item_id])
+end
+end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,3 +1,4 @@
 class Purchase < ApplicationRecord
 belongs_to :item
+has_one_attached :image
 end

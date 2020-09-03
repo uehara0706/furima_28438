b6e84f665108d82aacab198401607971a6ae8af9
uehara0706 <68547123+uehara0706@users.noreply.github.com>
2020-09-03 22:52:04 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'items#index'  
-  get 'posts', to: 'posts#index'
-  get 'items/new', to: 'items#new'
-  post 'items/new', to: 'items#new'
-  resources :items do
+  root "items#index"
+  resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
     resources :purchases, only:[:index, :create]
   end
-  
 end

f068723672dbbe1d3415f0814d415831df787dd3
uehara0706 <68547123+uehara0706@users.noreply.github.com>
2020-09-04 14:21:28 +0900

    商品出品機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,15 +15,12 @@ class ItemsController < ApplicationController
 
   def create
    
-    # Item.create(item_params)
     @item = Item.new(item_params)
-    if @item.save!
+    if @item.save
       redirect_to root_path
     else
       render :new
     end
-  
-    
   end
 
   def show

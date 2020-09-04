3367f9b1fe4447c19a813b3587946518d7e91913
uehara0706 <68547123+uehara0706@users.noreply.github.com>
2020-09-04 13:17:31 +0900

    basic認証の導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "items#index"
+  devise_for :users
   resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
     resources :purchases, only:[:index, :create]
   end
